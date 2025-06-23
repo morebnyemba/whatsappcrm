2684657cb513c73a610c5e6d810b694b7fb5319b
morebnyemba <morebnyemba@gmail.com>
2025-06-23 04:15:09 +0200

    feat(paynow_integration): add admin interface for Paynow configuration with single entry restriction

--- whatsappcrm_backend/paynow_integration/admin.py
+++ whatsappcrm_backend/paynow_integration/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import PaynowConfig
 
-# Register your models here.
+@admin.register(PaynowConfig)
+class PaynowConfigAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Paynow Configuration.
+    Ensures that only one configuration can be created.
+    """
+    list_display = ('integration_id', 'api_base_url')
+    fieldsets = (
+        (None, {
+            'fields': ('integration_id', 'integration_key', 'api_base_url'),
+            'description': "Enter the API credentials provided by Paynow. There should only be one configuration entry."
+        }),
+    )
+
+    def has_add_permission(self, request):
+        # Allow adding a new config only if one doesn't already exist.
+        return not PaynowConfig.objects.exists()
