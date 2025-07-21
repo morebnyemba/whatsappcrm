1527d4a62b751ce4c1c6a0910fd4118f0e5ae4e7
morebnyemba <morebnyemba@gmail.com>
2025-07-21 06:56:36 +0200

    Add admin interface for ReferralProfile model with enhanced display and search features

--- whatsappcrm_backend/referrals/admin.py
+++ whatsappcrm_backend/referrals/admin.py
@@ -1,3 +1,23 @@
+# whatsappcrm_backend/referrals/admin.py
 from django.contrib import admin
+from .models import ReferralProfile
 
-# Register your models here.
+@admin.register(ReferralProfile)
+class ReferralProfileAdmin(admin.ModelAdmin):
+    """
+    Admin view for the ReferralProfile model.
+    """
+    list_display = ('user', 'referral_code', 'get_referred_by_username', 'referral_bonus_applied')
+    search_fields = ('user__username', 'referral_code', 'referred_by__username')
+    list_filter = ('referral_bonus_applied',)
+    raw_id_fields = ('user', 'referred_by')
+    readonly_fields = ('referral_code',)
+
+    @admin.display(description='Referred By')
+    def get_referred_by_username(self, obj):
+        """
+        Displays the username of the referrer for a cleaner list view.
+        """
+        if obj.referred_by:
+            return obj.referred_by.username
+        return "N/A"
