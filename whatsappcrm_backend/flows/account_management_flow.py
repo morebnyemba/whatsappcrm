05c0f3252f2284433c17db7040bfee99ca764e40
morebnyemba <morebnyemba@gmail.com>
2025-07-22 10:45:45 +0200

    Add referral option to account management flow for user bonuses

--- whatsappcrm_backend/flows/account_management_flow.py
+++ whatsappcrm_backend/flows/account_management_flow.py
@@ -31,6 +31,7 @@ def create_account_management_flow():
                                             {"id": "account_deposit", "title": "Deposit Funds", "description": "Add money to your wallet"},
                                             {"id": "account_withdraw", "title": "Withdraw Funds", "description": "Request a withdrawal"},
                                             {"id": "account_check_balance", "title": "Check Balance", "description": "View your current balance"},
+                                            {"id": "account_refer_friend", "title": "Refer a Friend", "description": "Earn bonuses by inviting friends"},
                                         ]
                                     },
                                     {
@@ -52,6 +53,7 @@ def create_account_management_flow():
                     {"to_step": "switch_to_deposit", "condition_config": {"type": "interactive_reply_id_equals", "value": "account_deposit"}},
                     {"to_step": "switch_to_withdrawal", "condition_config": {"type": "interactive_reply_id_equals", "value": "account_withdraw"}},
                     {"to_step": "fetch_wallet_balance", "condition_config": {"type": "interactive_reply_id_equals", "value": "account_check_balance"}},
+                    {"to_step": "switch_to_referral", "condition_config": {"type": "interactive_reply_id_equals", "value": "account_refer_friend"}},
                     {"to_step": "switch_to_welcome", "condition_config": {"type": "interactive_reply_id_equals", "value": "account_back_to_main"}},
                 ]
             },
@@ -72,6 +74,14 @@ def create_account_management_flow():
                 "name": "switch_to_welcome",
                 "step_type": "action",
                 "config": {"actions_to_run": [{"action_type": "switch_flow", "trigger_keyword_template": "menu"}]},
+                "transitions": []
+            },
+            {
+                "name": "switch_to_referral",
+                "step_type": "action",
+                "config": {
+                    "actions_to_run": [{"action_type": "switch_flow", "trigger_keyword_template": "refer"}]
+                },
                 "transitions": []
             },
             # --- Balance Check Path ---
